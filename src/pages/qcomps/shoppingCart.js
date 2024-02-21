f9bb9e986709a04ee20c57981529c322039d3cd6
sristika <borasristika.new@gmail.com>
2024-02-20 19:42:44 -0500

    updates

--- src/pages/qcomps/shoppingCart.js
+++ src/pages/qcomps/shoppingCart.js
@@ -5,29 +5,23 @@ export default function RequestTracker() {
   const [completed, setCompleted] = useState(0);
 
   async function handleClick() {
-    setPending(pending + 1);
+    setPending((pending) => pending + 1);
     await delay(3000);
-    setPending(pending - 1);
-    setCompleted(completed + 1);
+    setPending((pending) => pending - 1);
+    setCompleted((completed) => completed + 1);
   }
 
   return (
     <>
-      <h3>
-        Pending: {pending}
-      </h3>
-      <h3>
-        Completed: {completed}
-      </h3>
-      <button onClick={handleClick}>
-        Buy
-      </button>
+      <h3>Pending: {pending}</h3>
+      <h3>Completed: {completed}</h3>
+      <button onClick={handleClick}>Buy</button>
     </>
   );
 }
 
 function delay(ms) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 }
