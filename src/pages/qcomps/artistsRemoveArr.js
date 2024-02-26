8f408f544eac0c7e2bf3f81b39a650a558bf3818
sristika <borasristika.new@gmail.com>
2024-02-26 12:53:35 -0500

    updates

--- src/pages/qcomps/artistsRemoveArr.js
+++ src/pages/qcomps/artistsRemoveArr.js
@@ -2,25 +2,29 @@ import { useState } from 'react';
 
 let initialArtists = [
   { id: 0, name: 'Marta Colvin Andrade' },
-  { id: 1, name: 'Lamidi Olonade Fakeye'},
-  { id: 2, name: 'Louise Nevelson'},
+  { id: 1, name: 'Lamidi Olonade Fakeye' },
+  { id: 2, name: 'Louise Nevelson' },
 ];
 
 export default function List() {
-  const [artists, setArtists] = useState(
-    initialArtists
-  );
+  const [artists, setArtists] = useState(initialArtists);
 
   return (
     <>
       <h1>Inspiring sculptors:</h1>
       <ul>
-        {artists.map(artist => (
+        {artists.map((artist) => (
           <li key={artist.id}>
             {artist.name}{' '}
-            <button onClick={() => {
-              artists.splice(artist.id, 1)
-            }}>
+            <button
+              onClick={() => {
+                setArtists(
+                  artists.filter((obj) => {
+                    return obj.id != artist.id;
+                  })
+                );
+              }}
+            >
               Delete
             </button>
           </li>
